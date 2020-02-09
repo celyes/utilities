d6f4455273edef15152965b160e1a100c11145dd
celyes <celyes02@gmail.com>
2020-02-09 18:27:07 +0100

    isntall and write test for file class

--- src/File.php
+++ src/File.php
@@ -5,9 +5,27 @@ namespace Celyes\Utilities;
 class File 
 {
 
+	protected $handle;
+	protected $size;
+	protected static $units = array('B','kB','MB','GB','TB','PB','EB','ZB','YB'); 
+	
 	public function __construct($handle)
 	{
-		echo $handle;
+		$this->handle = $handle;
+		$this->size = filesize($handle);
 	}
 
+	public function sizeForHumans($decimals = 2)
+	{
+
+		$factor = floor((strlen($this->size) - 1) / 3);	
+	    return sprintf("%.{$decimals}f", $this->size / 1024** $factor) . self::$units[$factor];
+
+	}
+
+	public static function getAllUnits()
+	{
+		return self::$units;
+	
+	}
 }
