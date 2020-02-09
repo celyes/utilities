cdf62bc1d5f37df869336e6fd855878478ea9190
celyes <celyes02@gmail.com>
2020-02-09 16:30:22 +0100

    configure autoload

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php 
+require 'vendor/autoload.php';
+
+use Celyes\Utilities\File;
+
+$file = new File("ilyes");
---
+++ src/File.php
@@ -0,0 +1,13 @@
+<?php 
+
+namespace Celyes\Utilities;
+
+class File 
+{
+
+	public function __construct($handle)
+	{
+		echo $handle;
+	}
+
+}
